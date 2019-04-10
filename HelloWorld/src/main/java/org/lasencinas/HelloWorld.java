6d1d0b49e08faf801915711a61c0ea0832033ad6
torrapipes <crbergas@gmail.com>
2019-04-10 22:34:35 +0200

    Method wave created, runned and it worked real nice

---
+++ HelloWorld/src/main/java/org/lasencinas/HelloWorld.java
@@ -0,0 +1,18 @@
+package org.lasencinas;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorld {
+	
+	@RequestMapping(path = "/")
+	@ResponseBody
+	public String wave() {
+		
+		return "Hello World!";
+		
+	}
+
+}
