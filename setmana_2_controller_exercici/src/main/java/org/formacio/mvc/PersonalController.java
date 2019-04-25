2d4dc0636c7f7d7f303aca0271103cc999adb748
torrapipes <crbergas@gmail.com>
2019-04-26 01:06:34 +0200

    Initial commit

---
+++ setmana_2_controller_exercici/src/main/java/org/formacio/mvc/PersonalController.java
@@ -0,0 +1,23 @@
+package org.formacio.mvc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class PersonalController {
+
+	// no toqueu la declaracio de baseDeDades ni el metode getBaseDeDades
+	// 
+	// als metodes que anau afegint, tracteu aquest array com si fos la base de dades
+	//
+	// per exemple, obtenir la persona amb id 1 sera fer baseDeDades.get(1), etc etc 
+	
+	private List<String> baseDeDades = new ArrayList<>(Arrays.asList("Joana","Antonia","Pere"));
+	
+	public List<String> getBaseDeDades() {
+		return baseDeDades;
+	}
+
+	// Poseu a partir d'aqui els vostre metodes
+}
