392cfd5170d44684cb72b5b61867d7243fd729f6
torrapipes <crbergas@gmail.com>
2019-04-26 18:37:32 +0200

    test_obtenir_amb_pathvariable passat

--- setmana_2_controller_exercici/src/main/java/org/formacio/mvc/PersonalController.java
+++ setmana_2_controller_exercici/src/main/java/org/formacio/mvc/PersonalController.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -39,5 +40,10 @@ public class PersonalController {
 		return this.getBaseDeDades().get(id);
 	}
 	
+	@RequestMapping(path="/persona/{id}")
+	@ResponseBody
+	public String obtenirNomAmbPath(@PathVariable int id) {
+		return this.getBaseDeDades().get(id);
+	}
 
 }
