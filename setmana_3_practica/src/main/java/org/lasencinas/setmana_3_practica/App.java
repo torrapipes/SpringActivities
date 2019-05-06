44cc1da43ba9d23c15d6ed58acc11bab013c6f20
torrapipes <crbergas@gmail.com>
2019-05-06 23:52:29 +0200

    Initial commit

---
+++ setmana_3_practica/src/main/java/org/lasencinas/setmana_3_practica/App.java
@@ -0,0 +1,13 @@
+package org.lasencinas.setmana_3_practica;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
