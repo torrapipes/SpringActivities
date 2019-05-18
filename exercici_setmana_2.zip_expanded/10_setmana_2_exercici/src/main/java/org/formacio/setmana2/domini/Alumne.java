bdf489f69c0c88e02f728e4c6fba2bea16d89334
torrapipes <crbergas@gmail.com>
2019-05-18 20:11:06 +0200

    passa casos test equals i subtipus

--- exercici_setmana_2.zip_expanded/10_setmana_2_exercici/src/main/java/org/formacio/setmana2/domini/Alumne.java
+++ exercici_setmana_2.zip_expanded/10_setmana_2_exercici/src/main/java/org/formacio/setmana2/domini/Alumne.java
@@ -25,5 +25,34 @@ public class Alumne {
 	public void setEdat(int edat) {
 		this.edat = edat;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Alumne))
+			return false;
+		Alumne other = (Alumne) obj;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
 	
 }
