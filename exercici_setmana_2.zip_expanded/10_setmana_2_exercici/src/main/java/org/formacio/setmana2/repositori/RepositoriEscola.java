78ec29c821c6191cbcbcb6921402fff3e9e8b0a8
torrapipes <crbergas@gmail.com>
2019-05-18 20:37:28 +0200

    cas test apunta_ok passat

--- exercici_setmana_2.zip_expanded/10_setmana_2_exercici/src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
+++ exercici_setmana_2.zip_expanded/10_setmana_2_exercici/src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
@@ -1,23 +1,46 @@
 package org.formacio.setmana2.repositori;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.formacio.setmana2.domini.Alumne;
 import org.formacio.setmana2.domini.Curs;
 import org.formacio.setmana2.domini.Matricula;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 /**
  * Modifica aquesta classe per tal que sigui un component Spring que realitza les 
  * operacions de persistencia tal com indiquen les firmes dels metodes
  */
+
+@Repository
 public class RepositoriEscola {
 
+	@PersistenceContext
+	EntityManager em;
+	
+	
 	
 	public Curs carregaCurs(String nom) {
-		return null;
+		return em.find(Curs.class, nom);
+	}
+	
+	public Alumne carregaAlumne(String nom) {
+		return em.find(Alumne.class, nom);
 	}
 	
 	
 	public Matricula apunta (String alumne, String curs) throws EdatIncorrecteException {
-	    return null;	
+		
+		Matricula mat = new Matricula();
+		mat.setAlumne(carregaAlumne(alumne));
+		mat.setCurs(carregaCurs(curs));
+		em.persist(mat);
+		return mat;
+		
 	}
+	
 	
 	
 }
